535aa6dd599b7573c6c82cdba0577c29fc7b09e9
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-02 17:32:29 +0300

    added internship main route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
+import internshipRoutes from "./routes/internship.routes.js"
 
 dotenv.config(); // Initializes dotenv so we can access variables using process.env
 
@@ -18,5 +19,6 @@ connectDB();
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("api/internship", internshipRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
