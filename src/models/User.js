9d1dc943766c91bfddc873a8ba2339662cecb87f
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-02 12:39:36 +0300

    created user model

---
+++ src/models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const memberCodes = [
+    "NNJ - 01 - Ex", "EMW - 02 - Ex", "SHL - 03 - Ex", "TSB - 04 - Mk",
+    "JBH - 05 - Mk", "RBC - 06 - Mk", "GAK - 07 - SD", "SHD - 08 - Sl",
+    "SBN - 09 - Fn", "JZD - 10 - HR", "SAD - 11 - HR", "KAN - 12 - D"
+];
+
+const userSchema = new mongoose.Schema({
+    role: {
+        type: String,
+        enum: ["intern", "internship", "member"],
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String
+    },
+    disabilityCardCode: {
+        type: String, 
+        required: function() { return this.role == "intern";}
+    },
+    disabilityType: {
+        type: String, 
+        required: function() { return this.role == "intern";}
+    },
+    memberCode: {
+        type: String,
+        required: function() { return this.role == "member";},
+        enum: memberCodes
+    },
+})
+
+export default mongoose.model("User", userSchema);
