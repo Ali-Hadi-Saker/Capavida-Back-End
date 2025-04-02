f1c002a4dc062f7a0ed10c0fb74e5dd2cebd3893
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-02 14:40:35 +0300

    created login function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -26,3 +26,21 @@ export const registerUser = async (req, res)=> {
         res.status(500).json({message: "Server error", error});
     }
 }
+
+export const loginUser = async (req, res)=> {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user) return res.status(404).json({message: "User not found"});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({message: "wrong credentials"});
+
+        const token = jwt.sign({id:user._id, role:user.role}, process.env.JWT_SECRET, {expiresIn: "1d"});
+
+        res.status(200).json({token, user});
+    } catch (error) {
+        res.status(500).json({message: "Server error", error});
+    }
+}
