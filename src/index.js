78b4c233ea6adc66d19d7bcfa5bd6adcda9763dc
Ali-Hadi-Saker <alihady530@gmail.com>
2025-04-22 17:17:41 +0300

    added chatbot routes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 import internshipRoutes from "./routes/internship.routes.js"
 import communityRoutes from "./routes/community.routes.js"
 import marketplaceRoutes from "./routes/marketplace.routes.js"
+import chatbotRoutes from "./routes/chatbot.routes.js"
 
 dotenv.config(); // Initializes dotenv so we can access variables using process.env
 
@@ -24,5 +25,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/internship", internshipRoutes)
 app.use("/api/communities", communityRoutes)
 app.use("/api/marketplace", marketplaceRoutes)
+app.use("/api/chatbot", chatbotRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
